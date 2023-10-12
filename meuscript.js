adaf1e1dc194c703421bff971b7886decf3361e3
Lucas Tauil <luctauil@gmail.com>
2023-10-11 21:17:46 -0300

    Primeiro Commit

---
+++ meuscript.js
@@ -0,0 +1,10 @@
+
+var num1 = prompt("Insira o primeiro numero");
+var num2 = prompt("Insira o segundo numero");
+
+var resultado = parseInt(num1) * parseInt(num2);
+
+
+
+console.log(resultado);
+alert(resultado);
